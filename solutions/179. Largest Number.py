b104427783c8fdccbe2e94fa62fc894cd4135362
Sandy <sandylamture03@gmail.com>
2021-08-21 20:20:19 +0530

    103 ms

--- solutions/179. Largest Number.py
+++ solutions/179. Largest Number.py
@@ -1,8 +1,23 @@
-class LargerNumKey(str):
-    def __lt__(x, y):
-        return x+y > y+x
-        
 class Solution:
-    def largestNumber(self, nums):
-        largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
-        return '0' if largest_num[0] == '0' else largest_num
+    def largestNumber(self, nums: List[int]) -> str:
+        if max(nums)==0:
+            return '0'
+        
+        for i in range(0,len(nums)):
+            nums[i]=str(nums[i])
+               
+        
+        ln=[nums[0]]
+        
+        for i in range(1,len(nums)):
+            placed=0
+            for j in range(0,len(ln)):
+                if nums[i]+ln[j]>=ln[j]+nums[i]:
+                    ln.insert(j,nums[i])
+                    placed=1
+                    break
+            if placed==0:
+                ln.append(nums[i])
+        
+        largest_number=''.join(ln)
+        return largest_number
